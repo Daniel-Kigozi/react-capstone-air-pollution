7e64843d92c53c23c2b5b36ab3fe5e2351ce8a1e
Daniel-Kigozi <kdanalex69@gmail.com>
2023-05-31 13:10:26 +0300

    fixed linter errors1

--- src/components/city.jsx
+++ src/components/city.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 const CityCard = () => {
-  let navigation = useNavigate();
+  const navigation = useNavigate();
   const handleNav = (city) => {
-    navigation(`${city}`)
-  }
-  return(
+    navigation(`${city}`);
+  };
+  return (
     <>
-    <h1>City Name</h1>
-    <h2>Country</h2>
-    <button type="button" className='view-polution-btn' onClick={() => handleNav('city')}>View-Pollution</button>
-  </>
-  )
-  
+      <h1>City Name</h1>
+      <h2>Country</h2>
+      <button type="button" className="view-polution-btn" onClick={() => handleNav('city')}>View-Pollution</button>
+    </>
+  );
 };
 
 export default CityCard;
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './components/home';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Pollution from './components/pollution'
+import Pollution from './components/pollution';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />
+    element: <HomePage />,
   },
   {
     path: ':city',
-    element: <Pollution />
+    element: <Pollution />,
   },
-])
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
