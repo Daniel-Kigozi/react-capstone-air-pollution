9d883ce88d73c1c0e8dede4b21fe9893434c00f5
Daniel-Kigozi <kdanalex69@gmail.com>
2023-05-31 13:05:26 +0300

    routes

--- src/components/city.jsx
+++ src/components/city.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
-const CityCard = () => (
-  <>
+const CityCard = () => {
+  let navigation = useNavigate();
+  const handleNav = (city) => {
+    navigation(`${city}`)
+  }
+  return(
+    <>
     <h1>City Name</h1>
     <h2>Country</h2>
-    <button type="button" className="view-polution-btn">
-      view polution
-    </button>
+    <button type="button" className='view-polution-btn' onClick={() => handleNav('city')}>View-Pollution</button>
   </>
-);
+  )
+  
+};
 
 export default CityCard;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App'
 import './index.css';
 import HomePage from './components/home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Pollution from './components/pollution'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />
+  },
+  {
+    path: ':city',
+    element: <Pollution />
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HomePage />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
