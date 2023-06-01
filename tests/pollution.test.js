1ae2cc8108c33da8585e19b510a952e286838b52
Daniel-Kigozi <kdanalex69@gmail.com>
2023-06-01 17:58:05 +0300

    Updates

--- tests/pollution.test.js
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import Pollution from './utils';
-
-test('should render Pollution', () => {
-  render(<Pollution />);
-});
